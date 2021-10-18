b4f4d717b1edae2dbedf1abbce57b9c2fba03fb0
Sergey Batukov <sashachepuha@gmail.com>
2021-10-18 07:16:25 +0000

    start

---
+++ src/components/utils/Flex.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+  component(:is='tag')
+    slot
+</template>
+
+<script>
+export default {
+  name: 'Flex',
+  props: {
+    tag: {
+      type: String,
+      default: 'div'
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
