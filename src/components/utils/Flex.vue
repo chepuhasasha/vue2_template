a0122e352a8d66d945d997d8d4278adf9865c274
Sergey Batukov <sashachepuha@gmail.com>
2021-10-18 08:00:54 +0000

    update flex template

--- src/components/utils/Flex.vue
+++ src/components/utils/Flex.vue
@@ -1,6 +1,10 @@
 <template lang='pug'>
-  component(:is='tag')
+  component(:is='tag' :style='getStyle')
     slot
+     .item
+     .item
+     .item
+     .item
 </template>
 
 <script>
@@ -10,11 +14,60 @@ export default {
     tag: {
       type: String,
       default: 'div'
+    },
+    col: {
+      type: Boolean,
+      default: false
+    },
+    gap: {
+      type: Number,
+      default: 10
+    },
+    width: {
+      type: String,
+      default: 'hug',
+      validator: (value) => {
+			  return ['hug', 'fill'].includes(value)
+		  }
+    },
+    height: {
+      type: String,
+      default: 'hug',
+      validator: (value) => {
+			  return ['hug', 'fill'].includes(value)
+		  }
+    },
+    justifyContent: {
+      type: String,
+      default: 'start',
+      // validator: (value) => {
+			//   return ['hug', 'fill'].includes(value)
+		  // }
+    },
+    alignItems: {
+      type: String,
+      default: 'start'
+    },
+  },
+  computed: {
+    getStyle() {
+      let map = { 
+        hug: 'max-content',
+        fill: '100%'
+      }
+      let result = {}
+      result.width = map[this.width]
+      result.height = map[this.height]
+      return result
     }
   }
 }
 </script>
 
-<style lang='scss'>
-
+<style lang='scss' scoped>
+.item {
+  width: 40px;
+  height: 40px;
+  background: rgb(37, 37, 37);
+}
 </style>
